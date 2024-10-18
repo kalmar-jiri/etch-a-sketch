2f26a4a5ee3cb99019907f38430495488011e679
Jiri Kalmar <kalmar.jiri@gmail.com>
2024-10-18 09:35:45 +0200

    Very basic sketching function

--- script.js
+++ script.js
@@ -2,18 +2,32 @@
 
 const container = document.querySelector('.container');
 const canvas = document.querySelector('.canvas');
+let squares;
 
-let gridSize = 16;
+let gridSize = 25;
 
-for (let col = 1; col <= gridSize; col++) {
-  const column = document.createElement('div');
-  column.classList.add('column');
+function createGrid(gridSize) {
+  for (let col = 1; col <= gridSize; col++) {
+    const column = document.createElement('div');
+    column.classList.add('column');
 
-  for (let square = 1; square <= gridSize; square++) {
-    const gridField = document.createElement('div');
-    gridField.classList.add('grid-field');
-    // gridField.textContent = `[${col},${square}]`;
-    column.appendChild(gridField);
+    for (let square = 1; square <= gridSize; square++) {
+      const gridField = document.createElement('div');
+      gridField.classList.add('grid-item');
+      // gridField.textContent = `[${col},${square}]`;
+      column.appendChild(gridField);
+    }
+    canvas.appendChild(column);
   }
-  canvas.appendChild(column);
+
+  squares = document.querySelectorAll('.grid-item');
+  console.log(squares);
 }
+
+createGrid(gridSize);
+
+squares.forEach(s => {
+  s.addEventListener('mouseenter', () => {
+    s.style.backgroundColor = 'black';
+  });
+});
