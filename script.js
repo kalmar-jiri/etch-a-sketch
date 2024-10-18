a523a7a329d338bf61191c9a71e8132f19aa349f
Jiri Kalmar <kalmar.jiri@gmail.com>
2024-10-18 09:10:44 +0200

    Setting up grid

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const container = document.querySelector('.container');
+const canvas = document.querySelector('.canvas');
+
+let gridSize = 16;
+
+for (let col = 1; col <= gridSize; col++) {
+  const column = document.createElement('div');
+  column.classList.add('column');
+
+  for (let square = 1; square <= gridSize; square++) {
+    const gridField = document.createElement('div');
+    gridField.classList.add('grid-field');
+    // gridField.textContent = `[${col},${square}]`;
+    column.appendChild(gridField);
+  }
+  canvas.appendChild(column);
+}
